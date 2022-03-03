71c1ad7a64f44b9bd052e91e25a6c55668097495
Abhilash <abhilash.m@card91.io>
2022-03-03 11:52:22 +0530

    first commit

---
+++ components/UIElements/NextLinkButton.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import NextLink, { LinkProps as NextLinkProps } from "next/link";
+
+interface NextLinkComposedProps
+  extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, "href">,
+    Omit<NextLinkProps, "href" | "as"> {
+  linkAs?: NextLinkProps["as"];
+  href: NextLinkProps["href"];
+}
+
+export const NextLinkComposed = React.forwardRef<
+  HTMLAnchorElement,
+  NextLinkComposedProps
+>(function NextLinkComposed(props, ref) {
+  const {
+    linkAs,
+    href,
+    replace,
+    scroll,
+    shallow,
+    prefetch,
+    locale,
+    children,
+    ...other
+  } = props;
+
+  return (
+    <NextLink
+      href={href}
+      prefetch={prefetch}
+      as={linkAs}
+      replace={replace}
+      scroll={scroll}
+      shallow={shallow}
+      passHref
+      locale={locale}
+    >
+      <a ref={ref} {...other}>
+        {children}
+      </a>
+    </NextLink>
+  );
+});
