92beb94c6b7b88849f38f416f84691e3ce5baad3
Abhilash <abhilash.m@card91.io>
2022-03-16 16:58:07 +0530

    added csp meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,7 @@ export default class MyDocument extends Document {
         <Head nonce={nonce}>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta httpEquiv="Content-Security-Policy" content={nonce} />
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {(this.props as any).emotionStyleTags}
         </Head>
